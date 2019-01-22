e0c93b52c46db45ec322462e1518563afb07e5e3
hamza5796 <mohamedhamza5796@gmail.com>
2019-01-22 15:43:44 +0200

    FahmyFix

---
+++ Love Not War/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float chaseSpeed;
+    public float Speed;
+    public Material material;
+    Renderer myrender;
+    Animator animator;
+    internal float wait;
+    internal bool canShoot;
+    internal bool inFiringRange;
+    internal GameObject  player;
+    Rigidbody mybody;
+    bool hitWall;
+    void onTransform()
+    {
+
+        myrender.material = material;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag != "Ground")
+        {
+            hitWall = true;
+
+        }
+    }
+    void followPlayer()
+    {
+     
+            gameObject.transform.LookAt(player.transform);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, chaseSpeed);
+        
+      
+
+
+    }
+    void Shoot()
+    { Instantiate(new Bullet(),transform); }
+    internal void playerDetected()
+    {
+        mybody.velocity = Vector3.zero;
+        if (!inFiringRange)
+        {
+            followPlayer();
+        }
+        else if (inFiringRange)
+        {
+            Shoot();
+        }
+
+    }
+    void patrol()
+    {
+        transform.Rotate(0, 90,0);
+        hitWall = false;
+    }
+    void Start()
+    {
+        wait = 0;
+        animator = GetComponent<Animator>();
+        myrender = gameObject.GetComponent<Renderer>();
+        Speed = 10;
+        chaseSpeed = 0.1f;
+        mybody = gameObject.GetComponent<Rigidbody>();
+       mybody.velocity = mybody.transform.forward*Speed;
+    }
+
+    void Update()
+    {
+        if (player)
+        {
+         
+                playerDetected();
+          
+        }
+        else
+        {
+            mybody.velocity = mybody.transform.forward * Speed;
+        }
+        if (hitWall)
+        {
+
+            patrol();
+        }
+    }
+
+ 
+}
