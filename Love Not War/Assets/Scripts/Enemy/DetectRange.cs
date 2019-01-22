774b2f6683ed4a3b52edbb542135eaa04abdc10d
FahmyHady <fahmy.hady@hotmail.com>
2019-01-22 14:03:57 +0200

    Enemy Stuff

---
+++ Love Not War/Assets/Scripts/Enemy/DetectRange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectRange : MonoBehaviour
+{
+    Enemy me;
+    private void Start()
+    {
+        me = GetComponentInParent<Enemy>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            me.player=other.gameObject;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            me.player = null;
+        }
+
+    }
+}
---
+++ Love Not War/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float chaseSpeed;
+    public float Speed;
+    public Material material;
+    Renderer myrender;
+    Animation surprised;
+
+    internal bool canShoot;
+    internal bool inFiringRange;
+    internal GameObject  player;
+    Rigidbody mybody;
+    bool hitWall;
+    void onTransform()
+    {
+
+        myrender.material = material;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag != "Ground")
+        {
+            hitWall = true;
+
+        }
+    }
+    void followPlayer()
+    {
+     
+            gameObject.transform.LookAt(player.transform);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, chaseSpeed);
+        
+      
+
+
+    }
+    void Shoot()
+    { Instantiate(new Bullet(),transform); }
+    internal void playerDetected()
+    {
+        mybody.velocity = Vector3.zero;
+        if (!inFiringRange)
+        {
+            followPlayer();
+        }
+        else if (inFiringRange)
+        {
+            Shoot();
+        }
+
+    }
+    void patrol()
+    {
+        transform.Rotate(0, 90,0);
+        hitWall = false;
+    }
+    void Start()
+    {
+        surprised = GetComponent<Animation>();
+        myrender = gameObject.GetComponent<Renderer>();
+        Speed = 10;
+        chaseSpeed = 0.1f;
+        mybody = gameObject.GetComponent<Rigidbody>();
+       mybody.velocity = mybody.transform.forward*Speed;
+    }
+
+    void Update()
+    {
+        if (player)
+        {
+            playerDetected();
+        }
+        else
+        { mybody.velocity = mybody.transform.forward * Speed;
+        }
+        if (hitWall)
+        {
+
+            patrol();
+        }
+    }
+
+ 
+}
